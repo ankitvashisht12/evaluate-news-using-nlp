6b9e6c4ff91bafb6ae36c716ac7208fce74b7efe
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-13 08:52:30 +0530

    use dotenv, change port to 8080 and send index.html file on root route

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.static("dist"))
+
+app.get("/", async function (req, res, next) {
+  res.sendFile("dist/index.html")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
