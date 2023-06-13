c065ac236d1b012ef64ce9fdc3f97f82a2383407
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-13 08:52:07 +0530

    import styles in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
 import { handleSubmit } from "./js/formHandler"
 import { checkForName } from "./js/nameChecker"
 
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 console.log("Importing has been done successfully!")
