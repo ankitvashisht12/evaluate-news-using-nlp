55ba936f38cbc5e01cf739b6f1f87aaf0eabc76c
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-17 19:24:44 +0530

    use cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,21 @@
 const express = require("express")
 const dotenv = require('dotenv')
+const cors = require("cors")
 
 dotenv.config()
 
 const app = express()
 const PORT = 8080;
 
+app.use(cors())
 app.use(express.static("dist"))
 
 app.get("/", async function (req, res, next) {
   res.sendFile("dist/index.html")
+})
+
+app.post("/analyze", async function (req, res, next) {
+  res.json({status: "success", polarity: "neutral", subjectivity: "objective", text: "Hello world"})
 })
 
 app.listen(PORT, () => {
