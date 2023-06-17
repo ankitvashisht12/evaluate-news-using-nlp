ffa013b55bb7b8400dd12af502dccf404c92a7fc
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-17 19:54:49 +0530

    write analyze api by using meaningcloud nlp api. Format file as well

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,57 @@
-const express = require("express")
-const dotenv = require('dotenv')
-const cors = require("cors")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const FormData = require("form-data");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 const PORT = 8080;
 
-app.use(cors())
-app.use(express.static("dist"))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("dist"));
 
 app.get("/", async function (req, res, next) {
-  res.sendFile("dist/index.html")
-})
+  res.sendFile("dist/index.html");
+});
 
 app.post("/analyze", async function (req, res, next) {
-  res.json({status: "success", polarity: "neutral", subjectivity: "objective", text: "Hello world"})
-})
+  const key = process.env.MEANING_CLOUD_LINCENSE_KEY;
+  const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+  const formData = new FormData();
+
+  const lang = req.query.lang || "en";
+  const model = req.query.model || "general";
+
+  formData.append("key", key);
+  formData.append("lang", lang);
+  formData.append("txt", req.body.text);
+  formData.append("model", model);
+
+  try {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    console.log("data", JSON.stringify(data, null, 2));
+    return res.json({
+      status: "success",
+      polarity: data.score_tag === "P" ? "POSITIVE" : "NEGATIVE",
+      subjectivity: data.subjectivity,
+      text: req.body.text,
+    });
+  } catch (err) {
+    console.error("Error in analyzing data", err);
+    return res.json({ status: "error", message: "Error in analyzing data" });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
