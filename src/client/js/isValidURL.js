68d5315661c85b436e581c40f6c81a7be0ff4375
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-18 13:11:43 +0530

    add method to check for validity of url

---
+++ src/client/js/isValidURL.js
@@ -0,0 +1,7 @@
+function isValidURL (url) {
+  if(!url) return
+  const regexPattern = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/
+  return regexPattern.test(url)
+}
+
+export { isValidURL }
