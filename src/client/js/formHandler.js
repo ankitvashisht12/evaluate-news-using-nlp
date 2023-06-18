a77310c0d09dd2fbc1838395804181278424deea
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-18 13:12:01 +0530

    use isValidURL method to check validity of url before passing to api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,27 +1,34 @@
 async function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    let textToAnalyze = document.getElementById('txt').value;
-    if(!textToAnalyze || (typeof textToAnalyze === "string" && !textToAnalyze?.trim()?.length)) return
+  let url = document.getElementById("url").value;
+  if (
+    !url ||
+    (typeof textToAnalyze === "string" && !url?.trim()?.length) ||
+    !Client.isValidURL(url)
+  ) {
+    alert("Please enter a valid URL");
+    return;
+  }
 
 
-    let response;
+  let response;
 
-    try {
-      response = await fetch('http://localhost:8080/analyze', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({text: textToAnalyze}),
-      })
-      response = await response.json()
-    }catch(err){
-      console.error("Error in analyzing data", err)
-      response = {status: "error", message: "Error in analyzing data"}
-    }
-    
-    Client.updateUI(response)
+  try {
+    response = await fetch("http://localhost:8080/analyze", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: textToAnalyze }),
+    });
+    response = await response.json();
+  } catch (err) {
+    console.error("Error in analyzing data", err);
+    response = { status: "error", message: "Error in analyzing data" };
+  }
+
+  Client.updateUI(response);
 }
 
-export { handleSubmit }
+export { handleSubmit };
