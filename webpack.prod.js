32cebe9dc76616da52250bfb3527e77af88d3934
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-18 14:25:13 +0530

    add service worker for offline functionality

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -11,6 +12,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
   },
   optimization: {
     minimize: true,
@@ -34,5 +37,6 @@ module.exports = {
       template: "./src/client/views/index.html",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new GenerateSW(),
   ],
 };
