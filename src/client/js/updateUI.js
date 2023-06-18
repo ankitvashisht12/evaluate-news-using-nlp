ec8c27a314c4f809596957142400d4ab1f00d2a4
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-06-18 13:40:12 +0530

    show result for both overall blog/article and individual texts result in list

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,19 +1,35 @@
-function updateUI (result) {
-  const resultElement = document.getElementById('results');
+function updateUI(result) {
+  const resultElement = document.getElementById("results");
 
-  if(!result || result.status !== "success") {
-    resultElement.innerText = "Error in analyzing data"
+  if (!result || result.status !== "success") {
+    resultElement.innerText = "Error in analyzing data";
   }
 
-  console.log("result", result)
+  const inDetailResult = result?.sentence_list?.map((sentence) => {
+    return `
+          <li class="result-item">
+            <p>Sentence: ${sentence.text}</p>
+            <p>Polarity: ${
+              sentence.score_tag === "P" ? "POSITIVE" : "NEGATIVE"
+            }</p>
+            <p>Confidence: ${sentence.confidence}%</p>
+          </li>
+        `;
+  });
 
   resultElement.innerHTML = `
-    <div>
+    <div class="overall-result">
+      <h3>Overall Analysis Results</h3>
       <p>Polarity: ${result.polarity}</p>
       <p>Subjectivity: ${result.subjectivity}</p>
-      <p>Text: ${result.text}</p>
     </div>
-  `
+    <div class="detail-result">
+      <h3>Analysis In Details</h3>
+      <ul>
+      ${inDetailResult.join("")}
+      </ul>
+    </div>
+  `;
 }
 
-export { updateUI }
+export { updateUI };
